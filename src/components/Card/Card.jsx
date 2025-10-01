614e873382047fd25ff5989ec4028f0e966ec3cf
Maxim Tsurcan <curicatusmaxus@gmail.com>
2025-10-01 23:10:25 +0300

    Initial commit

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,19 @@
+import './Card.css'
+
+export const Card = ({ id, title, price, adress, date, img }) => {
+    return (
+        <>
+            <a href="/product.html" className="card">
+                <div className="card--img">
+                    <img src={img} alt="product" />
+                </div>
+                <h5 className="card--title">{title}</h5>
+                <strong className="card--price">{price}</strong>
+                <div className="card--desc-box">
+                    <span className="card--desc">{adress}</span>
+                    <span className="card--desc">{date}</span>
+                </div>
+            </a>
+        </>
+    )
+}
---
+++ src/constants.js
@@ -0,0 +1,51 @@
+export const cardArray = [
+    {
+        id: 0,
+        title: 'Пвх материал 1й сорт',
+        price: '160 ₽',
+        adress: 'Казань, р-н Вахитовский',
+        date: '15 июля 11:39',
+        img: './images/productImage.png',
+    },
+    {
+        id: 1,
+        title: 'Пвх материал 2й сорт',
+        price: '170 ₽',
+        adress: 'Казань, р-н Вахитовский',
+        date: '10 июня 16:51',
+        img: './images/productImage.png',
+    },
+    {
+        id: 2,
+        title: 'Пвх материал 3й сорт',
+        price: '180 ₽',
+        adress: 'Казань, р-н Вахитовский',
+        date: '29 июля 09:11',
+        img: './images/productImage.png',
+    },
+    {
+        id: 3,
+        title: 'Пвх материал 4й сорт',
+        price: '190 ₽',
+        adress: 'Казань, р-н Вахитовский',
+        date: '30 июня 11:22',
+        img: './images/productImage.png',
+    },
+    {
+        id: 4,
+        title: 'Пвх материал 5й сорт',
+        price: '200 ₽',
+        adress: 'Казань, р-н Вахитовский',
+        date: '24 июля 18:39',
+        img: './images/productImage.png',
+    },
+    {
+        id: 5,
+        title: 'Пвх материал 6й сорт',
+        price: '210 ₽',
+        adress: 'Казань, р-н Вахитовский',
+        date: '1 июля 08:52',
+        img: './images/productImage.png',
+    },
+
+];
